774a396e723a01c7a072b005605815dfb190361e
o5xNo <asd38894268@gmail.com>
2023-03-24 01:54:01 +0800

    re

---
+++ homework5/truthTable.js
@@ -0,0 +1,23 @@
+function truthTable(a)
+{
+    var x=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    x.length=a;
+
+    for(var i=0;i<Math.pow(2,a);i++)
+    {
+        for(var j=a;j>0;j--)
+        {
+            if(x[j]===2)
+            {
+                x[j]=0;
+                x[j-1]++;
+            }
+        }
+            console.log(x);
+
+        x[a-1]++;
+
+    }
+}
+
+console.log(truthTable(4));
