82d1da46674e0dfe2ac91575423589f51c2b420e
o5xNo <asd38894268@gmail.com>
2023-03-31 10:50:58 +0800

    re

--- AI/GptHello.py
+++ AI/GptHello.py
@@ -3,7 +3,7 @@ import openai
 
 # Load your API key from an environment variable or secret management service
 # openai.api_key = os.getenv("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")
-openai.api_key = "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"
+openai.api_key = "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"
 response = openai.Completion.create(
     model="text-davinci-003", 
     prompt="GPT 是甚麼?", 
--- AI/tempCodeRunnerFile.py
+++ AI/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
+# Load your API key from an environment variable or secret management service
+# openai.api_key = os.getenv("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")
 openai.api_key = "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"
+response = openai.Completion.create(
+    model="text-davinci-003", 
+    prompt="GPT 是甚麼?", 
+    temperature=0, 
+    max_tokens=7)
