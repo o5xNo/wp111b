7f832017bd2fdc75bde008957d03838fc0ee4ee6
o5xNo <asd38894268@gmail.com>
2023-03-31 10:40:41 +0800

    re

--- AI/GptHello3.py
+++ AI/GptHello3.py
@@ -2,7 +2,8 @@ import os
 import openai
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("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")
+# openai.api_key = os.getenv("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")
+openai.api_key = "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"
 
 response = openai.Completion.create(
     model="text-davinci-003", 
---
+++ AI/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+openai.api_key = "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"
