6bd4ded470519ae840ea6a24989a80f123bf1929
o5xNo <asd38894268@gmail.com>
2023-04-14 10:52:40 +0800

    re

--- homework7/count.js
+++ homework7/count.js
@@ -1,5 +1,17 @@
 function count(times) {
-    
-    
-
+     let x={}
+     let y
+     for (y of times)
+     {
+        let z=x[y]
+        if (z==null)
+          x[y]=1
+        else
+          x[y]=x[y]+1
+     }
+    return x
   }
+
+  var times=['the','meet','over','cook','who','is','the','cook']
+  var x = count(times)
+  console.log(count(times));
