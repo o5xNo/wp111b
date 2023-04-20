177efedf93a9a916b932e841be0262f71ac03670
o5xNo <asd38894268@gmail.com>
2023-04-21 01:36:51 +0800

    re

--- homework7/mmul.js
+++ homework7/mmul.js
@@ -1,37 +1,44 @@
 class Matrix {
     constructor(a) {
         this.a = a 
-        var x=[[],[]]
 
     }
 
     add(b) {
 
-        for(var i=0;i<b.length;i++)
+        var x=[[],[]]
+
+        for(var i=0;i<this.a.length;i++)
         {
-            for(var j=0;j<b[0].length;j++)
+            for(var j=0;j<this.a[0].length;j++)
             {
-                x=this.a[i][j]+b[i][j];
+                x[i][j]=this.a[i][j]+b[i][j];
             }
         }
-        console.log(x);
+        return x;
     }
 
     mul(b) {
-        for(var i=0;i<b.length;i++)
+
+        var x=[[],[]]
+
+        for(var i=0;i<this.a.length;i++)
         {
-            for(var j=0;j<a[0].length;j++)
+            for(var j=0;j<b[0].length;j++)
             {
                 var c=0;
-                for(var t=0;t<b[0].length;t++)
+                for(var t=0;t<this.a[0].length;t++)
                 {
                     c=a[i][t]*b[t][j]+c;
                 }
                 x[i][j]=c;
             }
         }
-        console.log(x);
+        return x;
     }
 }
 
 var a=[[1,4],[2,3]],b=[[3,1],[5,2]];
+var x=new Matrix(a)
+console.log(x.add(b))
+console.log(x.mul(b))
