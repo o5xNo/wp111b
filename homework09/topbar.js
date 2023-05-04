3d94bc2dc77b5e6f73f598709767b595e7378262
o5xNo <asd38894268@gmail.com>
2023-05-04 21:26:46 +0800

    re

--- homework09/topbar.js
+++ homework09/topbar.js
@@ -1,12 +1,57 @@
-function myFunction() {
+function myFunction()
+  {
     document.getElementById("myDropdown").classList.toggle("show");
   }
-  window.onclick = function (e) {
-    if (!e.target.matches('.dropbtn')) {
+  window.onclick = function (e)
+   {
+    if (!e.target.matches('.dropbtn'))
+     {
       var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
+      if (myDropdown.classList.contains('show'))
+      {
         myDropdown.classList.remove('show');
       }
     }
   }
-  
+  let main = document.querySelector('#main')
+
+let pages = {
+    '#login':`
+<h1>登入</h1>
+<input type="text" placeholder="帳號"/><br/>
+<input type="text" placeholder="密碼"/><br/>
+<a href="#home">確認</a>
+`,
+    '#home':`
+`,
+    '#logout':`
+<a href="#home">登出</a>
+`,
+    '#signup':`
+    <h2>註冊</h2>
+
+    <p><input type="text" placeholder="姓名"/></p>
+    <p><input type="mail" placeholder="電子信箱或手機號碼"/></p>
+    <p><input type="password" placeholder="設定密碼"/></p>
+    
+    <label for="birthdaytime">出生年月日:</label><br>
+    <input required type="date"><br>
+    <br>
+    <label for="gender">性別:</label><br>
+    <input type="radio" name="gender" value="male">
+    <label for="male">男性</label>
+    <input type="radio" name="gender" value="female">
+    <label for="female">女性</label> 
+    <input type="radio" name="gender" value="other">
+    <label for="other">其他</label><br>
+    <br>
+    <input type="button" onclick="alert('恭喜你填寫這個詐欺表單，你的個資已成功外洩，恭喜!恭喜!')" value="送出">
+}
+`,
+}
+
+window.onhashchange = function () 
+{
+    let hash = window.location.hash
+    main.innerHTML = pages[hash]
+}
